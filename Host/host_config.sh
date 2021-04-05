11d927892fd9f083f101efebb26aa571dd9b4c48
Linfeng Zhong (Fred) <fred.zhong@outlook.com>
2021-04-05 20:29:58 +0800

    fix typo object --> objects

--- Host/host_config.sh
+++ Host/host_config.sh
@@ -12,20 +12,22 @@
 
 sudo
 cd /tmp
-wget --no-check-certificate https://github.com/linfengzhong/Nagios/archive/refs/tags/0.05.zip
-unzip 0.05.zip
-chmod 777 /tmp/Nagios-0.05/Libexec/check_*
+wget --no-check-certificate https://github.com/linfengzhong/Nagios/archive/refs/tags/0.06.zip
+unzip 0.06.zip
+chmod 777 /tmp/Nagios-0.06/Libexec/check_*
 mkdir /usr/local/nagios/etc/objects/myservers
 chmod 666 /usr/local/nagios/etc/objects/myservers
 chmod 666 /usr/local/nagios/etc/objects/myservers/*
 
-cp -p -f /tmp/Nagios-0.05/Libexec/check_* /usr/local/nagios/libexec
-cp -p -f /tmp/Nagios-0.05/Remote/nrpe.cfg /usr/local/nagios/etc/
-cp -p -f /tmp/Nagios-0.05/Host/nagios.cfg /usr/local/nagios/etc/
-cp -p -f /tmp/Nagios-0.05/Host/myservers/* /usr/local/nagios/etc/objects/myservers
+cp -p -f /tmp/Nagios-0.06/Libexec/check_* /usr/local/nagios/libexec
+cp -p -f /tmp/Nagios-0.06/Remote/nrpe.cfg /usr/local/nagios/etc/
+cp -p -f /tmp/Nagios-0.06/Host/nagios.cfg /usr/local/nagios/etc/
+cp -p -f /tmp/Nagios-0.06/Host/myservers/* /usr/local/nagios/etc/objects/myservers
 
 systemctl status nrpe
 systemctl restart nrpe
+
+/usr/local/nagios/bin/nagios -v /usr/local/nagios/etc/nagios.cfg
 
 systemctl status nagios 
 systemctl restart nagios
