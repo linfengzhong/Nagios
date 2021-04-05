96d0166430b294cd593231bbad8646063cbd6481
Linfeng Zhong (Fred) <fred.zhong@outlook.com>
2021-04-05 20:22:59 +0800

    Use cfg_dir to replace cfg_file

--- Host/host_config.sh
+++ Host/host_config.sh
@@ -1,13 +1,28 @@
 #!/usr/bin/env bash
+#
+#-rw------- (600)    只有拥有者有读写权限。
+#-rw-r--r-- (644)    只有拥有者有读写权限；而属组用户和其他用户只有读权限。
+#-rwx------ (700)    只有拥有者有读、写、执行权限。
+#-rwxr-xr-x (755)    拥有者有读、写、执行权限；而属组用户和其他用户只有读、执行权限。
+#-rwx--x--x (711)    拥有者有读、写、执行权限；而属组用户和其他用户只有执行权限。
+#-rw-rw-rw- (666)    所有用户都有文件读、写权限。
+#-rwxrwxrwx (777)    所有用户都有读、写、执行权限。
+#检测配置文件是否 OK 的命令
+#/usr/local/nagios/bin/nagios -v /usr/local/nagios/etc/nagios.cfg 
 
 sudo
 cd /tmp
-wget --no-check-certificate https://github.com/linfengzhong/Nagios/archive/refs/tags/0.04.zip
-unzip 0.04.zip
-chmod 777 /tmp/Nagios-0.04/Libexec/check_*
-cp -p -f /tmp/Nagios-0.04/Libexec/check_* /usr/local/nagios/libexec
-cp -p -f /tmp/Nagios-0.04/Remote/nrpe.cfg /usr/local/nagios/etc/
-cp -p -f /tmp/Nagios-0.04/Host/myservers /usr/local/nagios/etc/objects/
+wget --no-check-certificate https://github.com/linfengzhong/Nagios/archive/refs/tags/0.05.zip
+unzip 0.05.zip
+chmod 777 /tmp/Nagios-0.05/Libexec/check_*
+mkdir /usr/local/nagios/etc/objects/myservers
+chmod 666 /usr/local/nagios/etc/objects/myservers
+chmod 666 /usr/local/nagios/etc/objects/myservers/*
+
+cp -p -f /tmp/Nagios-0.05/Libexec/check_* /usr/local/nagios/libexec
+cp -p -f /tmp/Nagios-0.05/Remote/nrpe.cfg /usr/local/nagios/etc/
+cp -p -f /tmp/Nagios-0.05/Host/nagios.cfg /usr/local/nagios/etc/
+cp -p -f /tmp/Nagios-0.05/Host/myservers/* /usr/local/nagios/etc/objects/myservers
 
 systemctl status nrpe
 systemctl restart nrpe
