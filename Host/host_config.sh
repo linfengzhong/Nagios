2561946f3c2da92caac430857519beac62364850
Linfeng Zhong (Fred) <fred.zhong@outlook.com>
2021-04-05 20:02:52 +0800

    Change file organization

--- Host/host_config.sh
+++ Host/host_config.sh
@@ -2,12 +2,12 @@
 
 sudo
 cd /tmp
-wget --no-check-certificate https://github.com/linfengzhong/Nagios/archive/refs/tags/0.03.zip
-unzip 0.03.zip
-chmod 777 /tmp/Nagios-0.03/Libexec/check_*
-cp -p -f /tmp/Nagios-0.03/Libexec/check_* /usr/local/nagios/libexec
-cp -p -f /tmp/Nagios-0.03/Remote/nrpe.cfg /usr/local/nagios/etc/
-cp -p -f /tmp/Nagios-0.03/Host/windthroughwall.ml.cfg /usr/local/nagios/etc/objects
+wget --no-check-certificate https://github.com/linfengzhong/Nagios/archive/refs/tags/0.04.zip
+unzip 0.04.zip
+chmod 777 /tmp/Nagios-0.04/Libexec/check_*
+cp -p -f /tmp/Nagios-0.04/Libexec/check_* /usr/local/nagios/libexec
+cp -p -f /tmp/Nagios-0.04/Remote/nrpe.cfg /usr/local/nagios/etc/
+cp -p -f /tmp/Nagios-0.04/Host/myservers /usr/local/nagios/etc/objects/
 
 systemctl status nrpe
 systemctl restart nrpe
