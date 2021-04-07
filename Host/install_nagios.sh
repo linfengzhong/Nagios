8bb15fae56f883795f5fe29a80de6951de89527f
Linfeng Zhong (Fred) <fred.zhong@outlook.com>
2021-04-07 11:22:18 +0800

    Update install_nagios.sh
    new functions

--- Host/install_nagios.sh
+++ Host/install_nagios.sh
@@ -3,6 +3,71 @@
 # Author: Linfeng Zhong (Fred)
 # 2021-April-06 [Initial Version] - Shell Script for Nagios Core installing
 # Nagios Core - Installing Nagios Core From Source
+#
+#
+#fonts color
+Green="\033[32m"
+Red="\033[31m"
+Yellow="\033[33m"
+GreenBG="\033[42;37m"
+RedBG="\033[41;37m"
+Font="\033[0m"
+
+#notification information
+Info="${Green}[信息]${Font}"
+OK="${Green}[OK]${Font}"
+Error="${Red}[错误]${Font}"
+
+#从VERSION中提取发行版系统的英文名称，为了在debian/ubuntu下添加相对应的Nginx apt源
+VERSION=$(echo "${VERSION}" | awk -F "[()]" '{print $2}')
+
+check_system() {
+    if [[ "${ID}" == "centos" && ${VERSION_ID} -ge 7 ]]; then
+        echo -e "${OK} ${GreenBG} 当前系统为 Centos ${VERSION_ID} ${VERSION} ${Font}"
+        INS="yum"
+    elif [[ "${ID}" == "debian" && ${VERSION_ID} -ge 8 ]]; then
+        echo -e "${OK} ${GreenBG} 当前系统为 Debian ${VERSION_ID} ${VERSION} ${Font}"
+        INS="apt"
+        $INS update
+        ## 添加 Nginx apt源
+    elif [[ "${ID}" == "ubuntu" && $(echo "${VERSION_ID}" | cut -d '.' -f1) -ge 16 ]]; then
+        echo -e "${OK} ${GreenBG} 当前系统为 Ubuntu ${VERSION_ID} ${UBUNTU_CODENAME} ${Font}"
+        INS="apt"
+        $INS update
+    else
+        echo -e "${Error} ${RedBG} 当前系统为 ${ID} ${VERSION_ID} 不在支持的系统列表内，安装中断 ${Font}"
+        exit 1
+    fi
+
+    $INS install dbus
+
+    systemctl stop firewalld
+    systemctl disable firewalld
+    echo -e "${OK} ${GreenBG} firewalld 已关闭 ${Font}"
+
+    systemctl stop ufw
+    systemctl disable ufw
+    echo -e "${OK} ${GreenBG} ufw 已关闭 ${Font}"
+}
+
+is_root() {
+    if [ 0 == $UID ]; then
+        echo -e "${OK} ${GreenBG} 当前用户是root用户，进入安装流程 ${Font}"
+        sleep 3
+    else
+        echo -e "${Error} ${RedBG} 当前用户不是root用户，请切换到root用户后重新执行脚本 ${Font}"
+        exit 1
+    fi
+}
+judge() {
+    if [[ 0 -eq $? ]]; then
+        echo -e "${OK} ${GreenBG} $1 完成 ${Font}"
+        sleep 1
+    else
+        echo -e "${Error} ${RedBG} $1 失败${Font}"
+        exit 1
+    fi
+}
 #-----------------------------------------------------------------------------
 #===== RHEL 7/8 | CentOS 7/8 | Oracle Linux 7/8 =====
 #-----------------------------------------------------------------------------
